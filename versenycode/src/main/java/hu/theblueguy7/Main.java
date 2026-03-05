2808a32eef238a73f737315aeb51e406d620a032
TheBlueGuy7 <bartalis.hunor17@gmail.com>
2026-03-05 21:50:47 +0100

    Added javafx

--- versenycode/src/main/java/hu/theblueguy7/Main.java
+++ versenycode/src/main/java/hu/theblueguy7/Main.java
@@ -1,8 +1,14 @@
 package hu.theblueguy7;
 
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
+import static javafx.application.Application.launch;
+
 public class Main {
     public static void main(String[] args) {
-
     }
 }
